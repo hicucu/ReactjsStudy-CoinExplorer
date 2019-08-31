3ede1d62e8bd1b33ab4f6cabd1f283b1a6952490
hicucu <hicucu@gmail.com>
2019-08-31 18:52:23 +0900

    feat:coins

--- src/Routes/Coins/CoinContainer.js
+++ src/Routes/Coins/CoinContainer.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { apis } from "../../api";
 import CoinPresenter from "Routes/Coins/CoinPresenter";
 
 export default class extends React.Component {
+  state = {
+    coins: null,
+    error: null,
+    isLoading: true
+  };
+  async componentDidMount() {
+    try {
+      const { data: coins } = await apis.coins();
+      this.setState({ coins });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
   render() {
-    return <CoinPresenter />;
+    const { coins, error, isLoading } = this.state;
+    return <CoinPresenter coins={coins} error={error} isLoading={isLoading} />;
   }
 }
--- src/Routes/Coins/CoinPresenter.js
+++ src/Routes/Coins/CoinPresenter.js
@@ -1,8 +1,51 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const Coins = styled.section``;
+const Container = styled.section`
+  margin: 10px;
+`;
 
-const CoinPresenter = () => <Coins>Coins</Coins>;
+const Coin = styled.div`
+  margin: 10px;
+`;
+
+const Rank = styled.h1`
+  display: inline-block;
+  font-weight: 600;
+  margin-right: 10px;
+`;
+
+const Name = styled.span``;
+
+const Symbol = styled.span``;
+
+const compare = (a, b) => {
+  return a > b ? 1 : a < b ? -1 : 0;
+};
+
+const CoinPresenter = ({ coins, error, isLoading }) =>
+  isLoading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {coins
+        .filter(coin => coin.rank < 1001 && coin.rank > 0)
+        .sort((coinA, coinB) => compare(coinA.rank, coinB.rank))
+        .map(coin => (
+          <Coin key={coin.id}>
+            <Rank>#{coin.rank} </Rank>
+            <Symbol>{coin.symbol}</Symbol> / <Name>{coin.name}</Name>
+          </Coin>
+        ))}
+    </Container>
+  );
+
+CoinPresenter.prototype = {
+  coins: PropTypes.array,
+  error: PropTypes.string,
+  isLoading: PropTypes.bool.isRequired
+};
 
 export default CoinPresenter;
